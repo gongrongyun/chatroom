af38ce9c5dcbb54c39acf75d6c7568a6e68f2a39
gongrongyun <1716175849@qq.com>
2018-07-21 11:45:53 +0800

    sub document

---
+++ chat room.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $("#save").click(function(){
+        $.ajax({
+            type:"POST",
+            url:"#",
+            data:{
+                name:SESSION['name'],
+                content:$("#").val(),
+                status: 0,
+            },
+            dataType:"json",
+            success:function(){
+            },
+            error:function(jqXHR){
+                console.log("error:" + jqXHR.status);
+            }
+        });
+    })
+    setInterval(function(){
+        $.ajax({
+            type:"GET",
+            url:"",
+            data:{
+                name:"",
+                content:"",
+            },
+            dataType:"json",
+            success:function(data){
+                if(data.name == SESSION['name']){
+                    $("#room").append("<span class='class2'>"+ data.content +"</span>"+":"+data.name);
+                }
+                else{
+                    $("#room").append("<span class='class1'>"+ data.content +"</span>"+":"+data.name);
+                }
+                setInterval(function(){
+                    $("#room").append("<span class='class3'>"+data.datetime+"<span>")
+                },10*60*1000);
+            },
+            error:function(jqXHR){
+                console.log("error:" + jqXHR.status);
+            }
+        });
+    },500);
+    
+})
