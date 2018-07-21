75ed199ebf8261616ce693047f42d9d6dd5b74c7
wsx9721 <wsx9721@wsx9721deMacBook-Pro.local>
2018-07-21 16:16:05 +0800

    login and register

--- registerphp.php
+++ registerphp.php
@@ -7,33 +7,49 @@ $name=$_POST['name'];
 $psw=$_POST['psw'];
 $psw_confirm=$_POST['psw_confirm'];
 
-// $name="wsx9721";
-// $psw="123";
-// $psw_confirm="1234";
-
 $data=0;
 
 if ($name == "" || $psw == "" || $psw_confirm == "") {
     $data="请确认信息完整性";
 }
 else {
-    if($psw_confirm == $psw)
-    {
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=chat_room",$username,$password);
-            $sql = "INSERT INTO USER_INFOR_TABLE (name,password)
-            VALUES ('$name','$psw')";
-            $conn->exec($sql);
-            $data="注册成功";
+    try {
+        $pdo = new PDO("mysql:host=$servername;dbname=chat_room",$username,$password);
+        $sql = "SELECT * FROM USER_INFOR_TABLE WHERE name = '$name'";
+        $res=$pdo->prepare($sql);
+        // $res->bindValue('id',$name);
+        $res->execute();
+        $result=$res->fetch(PDO::FETCH_ASSOC);
+        $pdo=null;
+        if ($result) {
+            $data="用户名已存在";
         }
-        catch(PDOException $e)
-        {
-            $data="数据库连接失败".$e->getMessage();
-        }
+        else {
+            if ($result) {
+                $data="用户名已存在";
+            }
+            else {
+                if($psw_confirm == $psw)
+                {
+                    try {
+                        $pdo = new PDO("mysql:host=$servername;dbname=chat_room",$username,$password);
+                        $sql = "INSERT INTO USER_INFOR_TABLE (name,password)
+                        VALUES ('$name','$psw')";
+                        $pdo->exec($sql);
+                        $data="注册成功";
+                    }
+                    catch(PDOException $e) {
+                        $data="1#数据库连接失败".$e->getMessage();
+                    }
 
-    }
-    else {
-        $data="两次输入密码不一致";
+                }
+                else {
+                    $data="两次输入密码不一致";
+                }
+            }
+        }
+    } catch (PDOException $e) {
+        $data="2#数据库连接失败".$e->getMessage();
     }
 }
 
