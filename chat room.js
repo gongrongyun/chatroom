df4a8ac9ad2b321b2bfdac0b2d1688b0b9232fae
gongrongyun <1716175849@qq.com>
2018-07-22 23:21:55 +0800

    id is not solve

--- chat room.js
+++ chat room.js
@@ -1,55 +1,61 @@
 $(document).ready(function(){
     var username = document.cookie.split(";")[0].split("=")[1];
+    console.log(username);
     $("#save").click(function(){
-        var mydate = new Date();
+        if(!username){
+            alert("请先登录");
+        }
+        else {
+            var mydate = new Date();
+            $.ajax({
+                type:"POST",
+                url:"conn_chatroom.php",
+                data:{
+                    name : username,
+                    content : $("#input").val(),
+                    time : mydate.toLocaleDateString(),
+                },
+                dataType:"text",
+                success:function(){
+                    console.log("相信你自己 没有bug");
+                },
+                error:function(jqXHR){
+                    console.log("error:" + jqXHR.status);
+                }
+            });
+        }
+    })
+    var id = 0;
+    setInterval(function(){
         $.ajax({
-            type:"POST",
-            url:"conn_chatroom.php",
+            type:"GET",
+            url:"response.php",
             data:{
-                name : username,
-                content : $("#input").val(),
-                time : mydate.toLocaleDateString(),
+                name:"",
+                content:"",
+                time:"",
+                last_id:id,
             },
-            dataType:"text",
-            success:function(){
-                console.log("asjf");
+            dataType:"json",
+            success:function(data){
+                for(var i = 0; i < data.length; i++){
+                if(data[i].name === username){
+                    $("#room").append("<span class='class2'>"+ data[i].content +":"+data[i].name+"</span>");
+                }
+                else{
+                    $("#room").append("<span class='class1'>"+ data[i].name+":"+data[i].content +"</span>");
+                }
+                id = data[data.length-1].id;
+            }
+                setInterval(function(){
+                    $("#room").append("<span class='class3'>"+data[0].time+"<span>")
+                },10*60*1000);
             },
             error:function(jqXHR){
                 console.log("error:" + jqXHR.status);
-            }
+            },
         });
-    })
-    // var id = 0;
-    // setInterval(function(){
-    //     $.ajax({
-    //         type:"GET",
-    //         url:"response.php",
-    //         data:{
-    //             name:"",
-    //             content:"",
-    //             time:"",
-    //             last_id:id,
-    //         },
-    //         dataType:"json",
-    //         success:function(data){
-    //             for(var i = 0; i < data.length; i++){
-    //             if(data[i].name === username){
-    //                 $("#room").append("<span class='class2'>"+ data[i].content +":"+data[i].name+"</span>");
-    //             }
-    //             else{
-    //                 $("#room").append("<span class='class1'>"+ data[i].name+":"+data[i].content +"</span>");
-    //             }
-    //             id = data[data.length-1].id;
-    //         }
-    //             setInterval(function(){
-    //                 $("#room").append("<span class='class3'>"+data[0].time+"<span>")
-    //             },10*60*1000);
-    //         },
-    //         error:function(jqXHR){
-    //             console.log("error:" + jqXHR.status);
-    //         },
-    //     });
-    // },500);
+    },500);
 
     // $.ajax({
     //     type:"GET",
