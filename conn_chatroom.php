3aa2822f25ff0c48727ec26212ddc1dfad7a3b35
KB-mfl <37804131+KB-mfl@users.noreply.github.com>
2018-07-24 18:48:58 +0800

    php

--- conn_chatroom.php
+++ conn_chatroom.php
@@ -10,31 +10,33 @@
         echo $sql . "<br>" . $e->getMessage();
     }
     // 存入聊天数据
-    // if(isset($_POST["name"])&&isset($_POST["content"])&&isset($_POST["time"])){
-    $name = $_POST["name"];
-    $content = $_POST["content"];
-    $flag = 0;
-    if($name === $_SESSION['name']){
-        $flag = 1;
+    if(isset($_POST["name"]) && isset($_POST["content"]) && isset($_POST["time"])){
+      $name = $_POST["name"];
+      $content = $_POST["content"];
+      $flag = 0;
+      if($name === $_SESSION['name']){
+          $flag = 1;
+      }
+      if($flag){
+          $insert = $conn->prepare("INSERT INTO chat_room_table (name,content,time)VALUES(:name,:content,NOW())");
+                  $insert -> bindValue(":name",$name);
+                  $insert -> bindValue(":content",$content);
+                  $insert -> execute();
+          // $message=array();
+          // $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+          // $conn->bindValue("name:",name);
+          // $sql="INSERT INTO chat_room_table (name,content,status) VALUES ("$name","$content",$status)";
+          // $conn->exec($sql);
+          //   $data = "success";
+          //   echo json_encode($data);
+          // }
+          // else {
+          //     echo "fcdcw";
+          // }
+      }
+      else {
+          echo "请你不要皮";
+      }
     }
-    if($flag){
-        $insert = $conn->prepare("INSERT INTO chat_room_table (name,content,time)VALUES(:name,:content,NOW())");
-                $insert -> bindValue(":name",$name);
-                $insert -> bindValue(":content",$content);
-                $insert -> execute();
-        // $message=array();
-        // $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        // $conn->bindValue("name:",name);
-        // $sql="INSERT INTO chat_room_table (name,content,status) VALUES ("$name","$content",$status)";
-        // $conn->exec($sql);
-        //   $data = "success";
-        //   echo json_encode($data);
-        // }
-        // else {
-        //     echo "fcdcw";
-        // }
-    }
-    else {
-        echo "请你不要皮";
-    }
+
 ?>
