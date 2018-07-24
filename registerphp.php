8031446b3319bb1ddaf4e4e457962ad49ce1e66c
wsx9721 <924708077@qq.com>
2018-07-24 18:54:13 +0800

    wo jue de gai wan le

--- registerphp.php
+++ registerphp.php
@@ -10,46 +10,41 @@ catch(PDOException $e) {
     header(http_response_code(500));
     die(json_encode($data));
 }
-session_start();
 
+if(isset($_POST['name'])&&isset($_POST['psw'])) {
 
-if(isset($_POST['name'])) {
+    session_start();
+
     $name=$_POST['name'];
-}
-$psw=$_POST['psw'];
-$psw_confirm=$_POST['psw_confirm'];
+    $psw=$_POST['psw'];
 
-if ($name == "" || $psw == "" || $psw_confirm == "") {
-    $data="请确认信息完整性";
-    header(http_response_code(401));
-    die(json_encode($data));
-}
-else {
-    $sql = "SELECT * FROM USER_INFOR_TABLE WHERE name = '$name'";
-    $res=$pdo->prepare($sql);
-    // $res->bindValue('id',$name);
-    $res->execute();
-    $result=$res->fetch(PDO::FETCH_ASSOC);
-    if ($result) {
-        $data="用户名已存在";
-        header(http_response_code(422));
+    if ($name == "" || $psw == "") {
+        $data="请确认信息完整性";
+        header(http_response_code(401));
         die(json_encode($data));
     }
     else {
-        // if($psw_confirm == $psw)
-        // {
-        $sql = "INSERT INTO USER_INFOR_TABLE (`name`, `pwd`)
-        VALUES ('$name','$psw')";
-        $pdo->exec($sql);
-        echo json_encode("注册成功");
-        $_SESSION['name']=$name;
-
-        // }
-        // else {
-        //     header(http_response_code(422));
-        //     $data="两次输入密码不一致";
-        // }
+        $sql = "SELECT * FROM USER_INFOR_TABLE WHERE name = :name";
+        $res=$pdo->prepare($sql);
+        $res->bindValue(':name',$name);
+        $res->execute();
+        $result=$res->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            $data="用户名已存在";
+            header(http_response_code(422));
+            die(json_encode($data));
+        }
+        else {
+            $sql = "INSERT INTO USER_INFOR_TABLE (`name`, `pwd`)
+            VALUES (:name,:psw)";
+            $res=$pdo->prepare($sql);
+            $res->bindValue(':name',$name);
+            $res->bindValue(':psw',$psw);
+            $res->execute();
+            $pdo->exec($sql);
+            echo json_encode("注册成功");
+            $_SESSION['name']=$name;
+        }
     }
 }
-// echo $data;
  ?>
